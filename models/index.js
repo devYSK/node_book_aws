58295eebb8a44c1fee6bd9478cab16e593424575
Young Soo Kim <41531594+devYSK@users.noreply.github.com>
2021-08-18 17:46:12 +0900

    models/index.js수정

--- models/index.js
+++ models/index.js
@@ -1,15 +1,11 @@
-'use strict';
-
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config')[env];
 const User = require('./user');
 const Post = require('./post');
 const Hashtag = require('./hashtag');
 
-
 const db = {};
-
 const sequelize = new Sequelize(
     config.database, config.username, config.password, config,
 );
